6cdc760f0ef8bf6dacbb6bdb8e5159f399eff348
d_bulyga <d.bulyga@btslogistics.by>
2024-09-09 11:13:38 +0300

    base constants

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <Arduino.h>
+#include <FastLED.h>
 
+#define POWER_SENSOR_PIN 4
+#define POWER_LED_PIN    2
+#define NUM_LEDS         1
+
+#define LED_TYPE         WS2812B
+#define COLOR_ORDER      GRB
+
+CRGB leds[NUM_LEDS];
+CRGB colors[] = {CRGB::Red, CRGB::Green};
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
+    // Настройка пинов
+  pinMode(POWER_SENSOR_PIN, INPUT);  // Пин сенсора как вход
+  pinMode(POWER_LED_PIN, OUTPUT);    // Пин светодиода как выход
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
