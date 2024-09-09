c07dd04678a4c458aa3c1dabc35b29b5fe04d5e3
d_bulyga <d.bulyga@btslogistics.by>
2024-09-09 11:41:52 +0300

    Wifi dep

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <WiFi.h>
 
 #define POWER_SENSOR_PIN 4
 #define POWER_LED_PIN    2
@@ -8,16 +9,23 @@
 #define LED_TYPE         WS2812B
 #define COLOR_ORDER      GRB
 
+// Задайте имя и пароль вашей Wi-Fi сети
+const char* ssid = "your_SSID";      // Замените на имя вашей Wi-Fi сети
+const char* password = "your_PASSWORD"; // Замените на пароль вашей Wi-Fi сети
+
 CRGB leds[NUM_LEDS];
 CRGB colors[] = {CRGB::Red, CRGB::Green};
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-    // Настройка пинов
+  // Настройка пинов
   pinMode(POWER_SENSOR_PIN, INPUT);  // Пин сенсора как вход
-  pinMode(POWER_LED_PIN, OUTPUT);    // Пин светодиода как выход
-  // put your setup code here, to run once:
+  FastLED.addLeds<LED_TYPE, POWER_LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // Изначально все светодиоды выключены (черный цвет)
+  FastLED.clear();
+  FastLED.show();
   int result = myFunction(2, 3);
 }
 
